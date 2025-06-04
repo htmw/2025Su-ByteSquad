56a2ed15490dfb12899ccb030664afe095caaa59
Jia Lau <90795123+jeeweelee@users.noreply.github.com>
2025-06-04 16:50:47 -0400

    Initial commit - replace with new project

---
+++ src/main/java/com/bytesquad/supplementaistore/SupplementAiStoreApplication.java
@@ -0,0 +1,13 @@
+package com.bytesquad.supplementaistore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplementAiStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplementAiStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytesquad/supplementaistore/SupplementAiStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytesquad.supplementaistore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplementAiStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
